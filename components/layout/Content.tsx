0fea946b95ddf2107cf0fa54c756e2c370b65a10
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-11-24 00:34:11 +0300

    feat: add spacing and color for cards

--- components/layout/Content.tsx
+++ components/layout/Content.tsx
@@ -3,13 +3,13 @@ import {Grid} from "@material-ui/core";
 import PCard from "../PCard";
 
 const styles = {
-  Paper: { padding: 20, marginTop: 10, marginBottom: 10, background: 'gray'}
+  Paper: { padding: 20, marginTop: 10, marginBottom: 10, background: 'lightgray'}
 }
 
 export const Content = () =>
   <Grid container justify="center" spacing={10}>
     <Grid item>
-      <PCard title='First title' content='First content' styles={styles}/>
+      <PCard title='First title!!!' content='First content!!!' styles={styles}/>
     </Grid>
     <Grid item>
       <PCard title='Second title' content='Second content' styles={styles}/>
